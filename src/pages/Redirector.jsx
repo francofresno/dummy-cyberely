6e9672e01b11c6970b99266a31749bc8fb6c7910
franco fresno <franco.fresno@gmail.com>
2022-11-03 20:14:34 -0300

    deploy

--- src/pages/Redirector.jsx
+++ src/pages/Redirector.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Spin } from "antd";
+import { URL_FRONT } from "../utils/url";
+
+const whitelist = ["https://forms.gle/m1isNQdp6kX4MxY17"];
 
 const Redirector = () => {
   let location = useLocation();
@@ -9,6 +12,12 @@ const Redirector = () => {
   useEffect(() => {
     if (!location) return;
     const dest = location.search.substring(6);
+    const estaEnWhitelist = whitelist.some((pagina) => pagina === dest);
+    if (!estaEnWhitelist) {
+      window.location.replace(URL_FRONT);
+      return;
+    }
+
     window.location.replace(dest);
   }, [location]);
 
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,6 @@
-export const URL = "https://cyberely-vulns.onrender.com";
+// export const URL = "https://cyberely-vulns.onrender.com";
 // export const URL = "https://cyberely-back.onrender.com";
-// export const URL = "http://localhost:5000";
+export const URL = "http://localhost:5000";
+export const URL_FRONT = "https://cyberely-vuln.onrender.com";
+
+// export const URL_FRONT = "http://localhost:3000/";
